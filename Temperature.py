2c20c2df46ec36962df91ca2e63d94a1ee09c5ee
Brian Jefferson <92610900+CushuN@users.noreply.github.com>
2023-07-18 00:14:54 -0500

    Create Temperature.py
    Code that will change Fahrenheit to Celsius.

---
+++ Temperature.py
@@ -0,0 +1,7 @@
+# ACloudGuru Lab (Fahrenheit to Celsius)
+
+temp = float(input('What temperature (in Fahrenheit) would  you like converted to Celsius? '))
+
+celsius = (temp - 32) * 5 / 9 
+
+print(str(temp), 'is', celsius)
