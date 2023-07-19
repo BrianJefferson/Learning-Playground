4623af13122eb1a57eec4b2c3d234e72a2dc0c44
Brian Jefferson <92610900+CushuN@users.noreply.github.com>
2023-07-19 00:44:18 -0500

    Create Lists.py

---
+++ Lists.py
@@ -0,0 +1,15 @@
+# ACloudGuru Lab | Lists
+
+my_list = [1, 2, 3, 4, 5]
+
+my_list[0] = 'a'
+
+print(my_list + [8, 9, 10])
+
+del my_list[0]
+
+print(my_list + [8, 9, 10])
+
+my_list.append(8)
+
+print(my_list)
