3f9b87dbbf50d756e6c5d08095f02abbe99c17b1
Brian Jefferson <92610900+CushuN@users.noreply.github.com>
2023-07-18 23:51:47 -0500

    Update Temperature.py

--- Temperature.py
+++ Temperature.py
@@ -1,6 +1,6 @@
 # ACloudGuru Lab (Fahrenheit to Celsius)
 
-temp = float(input('What temperature (in Fahrenheit) would  you like converted to Celsius? '))
+temp = float(input('What temperature (in Fahrenheit) would  you like converted into Celsius? '))
 
 celsius = (temp - 32) * 5 / 9 
 
