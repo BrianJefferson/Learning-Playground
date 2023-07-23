b94f0bea3a884e4a47a28805cefac711a02fa524
Brian Jefferson <92610900+CushuN@users.noreply.github.com>
2023-07-23 00:57:07 -0500

    Create conditional statements.py

---
+++ conditional statements.py
@@ -0,0 +1,30 @@
+# ACloudGuru Lab/Practice | "Conditionals"
+
+if 'a' > 'b':
+    print('CONDITION was true')
+elif 'a' == 'b':
+    print('It is equal')
+else:
+    print('CONDTION was false')
+
+
+name = "Brian"
+
+if name == "chris":
+    print('Hello chris')
+else:
+    pass
+
+if name == "chris":
+    print('Hello chris')
+else:
+    print('Wrong user name! ')
+
+id = int(input('What is your age? '))
+
+if id > 18:
+    print('You are allowed to get in! ')
+elif id == 18:
+    print('Wow you just made it! ')
+else:
+    print('You can not get into the group! ')
