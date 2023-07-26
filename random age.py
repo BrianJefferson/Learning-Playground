52c47386304059208db3b13818c92b5f27873a46
Brian Jefferson <92610900+BrianJefferson@users.noreply.github.com>
2023-07-26 18:08:49 -0500

    Create random age.py

---
+++ random age.py
@@ -0,0 +1,12 @@
+# ACloudGuru Lab/Practice 
+
+age = int(input("How old are you? "))
+
+if age < 18:
+ print("Young person")
+if age > 18 and age <=40:
+ print("Adult")
+if age > 40 and age <= 70:
+ print("Getting wiser")
+if age >= 70:
+ print("You look fantastic!")
