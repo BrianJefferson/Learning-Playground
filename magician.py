6f0dabdcf02813b5feda818f9e7ae568810c33ca
Brian Jefferson <92610900+BrianJefferson@users.noreply.github.com>
2023-08-09 18:23:36 -0500

    Create magician.py

---
+++ magician.py
@@ -0,0 +1,11 @@
+is_magician = True
+is_expert = False
+
+if is_magician and is_expert == True:
+    print('You are a master magician')
+
+elif is_magician:
+    print("at least you're getting there")
+
+else:
+    print("You need magic powers")
