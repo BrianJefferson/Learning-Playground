08a97fa42bbd3954be1483dba0abbe80dee6edc7
Brian Jefferson <92610900+BrianJefferson@users.noreply.github.com>
2023-08-10 21:42:41 -0500

    Create How_are_you.py

---
+++ How_are_you.py
@@ -0,0 +1,8 @@
+#Functions
+
+feeling = input("how are you? ")
+
+def say_hello(name, feeling):
+    print(f'helloooo {name}, Glad you are feeling {feeling}! Hope your day continues to be great!')
+
+say_hello('Brian', feeling)
