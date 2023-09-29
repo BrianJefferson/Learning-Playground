a164f9186b7857ae781f5973aedd6a57d7a3796f
Brian Jefferson <92610900+BrianJefferson@users.noreply.github.com>
2023-09-29 16:33:48 -0500

    Create make_your_own_qrcode.py

---
+++ make_your_own_qrcode.py
@@ -0,0 +1,19 @@
+#You will need to install "qrcode Image" extension before using this code. 
+
+import qrcode
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("https://www.google.com") # Change the URL on this line to your own.
