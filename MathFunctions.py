244eb65f81245fec7548901f5d6177799bde02b5
Brian Jefferson <92610900+BrianJefferson@users.noreply.github.com>
2023-10-07 10:49:55 -0500

    Create MathFunctions.py

---
+++ MathFunctions.py
@@ -0,0 +1,25 @@
+import math as m
+
+#ceil(), floor(), trunc(), pi()
+
+print(m.ceil(3.6))
+print(m.floor(3.6))
+print(m.trunc(3.6))
+print(m.pi)
+
+
+# factorial()
+# 3! = 3 * 2 * 1 = 6
+# 4! = 4 * 3 * 2 * 1 = 24
+
+print(m.factorial(3))
+print(m.factorial(4))
+
+# sqrt()
+print(m.sqrt(16))
+print(m.sqrt(100))
+
+# hypot()
+
+print(m.hypot(3, 4))
+print(m.hypot(6, 12))
